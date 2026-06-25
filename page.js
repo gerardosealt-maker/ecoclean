a71ebae1b5d79b00b9fab2f7ecd693107b91d512
gerardosealt-maker <gerardosealt@gmail.com>
2026-06-25 13:54:48 -0600

    Delete page.js

--- page.js
+++
@@ -1,3 +0,0 @@
-import { NextResponse } from 'next/server';
-import { COOKIE } from '../../../lib/server';
-export async function POST(){ const res=NextResponse.json({ok:true}); res.cookies.set(COOKIE,'',{path:'/',maxAge:0}); return res; }
